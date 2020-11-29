3562b7a1b9c193aa8b75b894a5c1669c5217f14e
Rickard Natt och Dag <rickard@hey.com>
2020-11-29 17:01:31 +0100

    refactor: remove unnecessary function

--- rust-2017/day_01/src/lib.rs
+++ rust-2017/day_01/src/lib.rs
@@ -1,17 +1,11 @@
 pub type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
 
-fn parse_value(value: &char) -> u32 {
-    value.to_digit(10).unwrap()
-}
-
 pub fn part_01(input: &Vec<char>) -> Result<u32> {
     let mut sum = 0;
 
     for (i, v) in input.iter().enumerate() {
         match (input.get(0), input.get(i + 1)) {
-            (Some(v1), None) | (_, Some(v1)) if v == v1 => {
-                sum += parse_value(v);
-            }
+            (Some(v1), None) | (_, Some(v1)) if v == v1 => sum += v.to_digit(10).unwrap(),
             _ => (),
         }
     }
