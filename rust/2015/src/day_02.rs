c927f0204a8649e0fb878bdf9fd1c16be5cf0f1c
Rickard Natt och Dag <rickard@nattochdag.dev>
2021-11-28 11:41:48 +0100

    refactor(2015): simplify day 2

--- rust/2015/src/day_02.rs
+++ rust/2015/src/day_02.rs
@@ -39,22 +39,18 @@ All numbers in the elves' list are in feet. How many total square feet of wrappi
 /// ```
 #[aoc(day2, part1)]
 pub fn solve_part_01(input: &[(u32, u32, u32)]) -> u32 {
-    // 2*l*w + 2*w*h + 2*h*l
-    let mut total_paper = 0;
-
-    for (length, width, height) in input {
+    input.iter().fold(0, |sum, (length, width, height)| {
         let first_side = length * width;
         let second_side = width * height;
         let third_side = height * length;
         let mut all_sides = [first_side, second_side, third_side];
         all_sides.sort_unstable();
 
-        total_paper += 2 * first_side + 2 * second_side + 2 * third_side + all_sides[0];
-    }
-
-    total_paper
+        2 * first_side + 2 * second_side + 2 * third_side + all_sides[0] + sum
+    })
 }
 /* Part Two
+
 The elves are also running low on ribbon. Ribbon is all the same width, so they only have to worry about the length they need to order, which they would again like to be exact.
 
 The ribbon required to wrap a present is the shortest distance around its sides, or the smallest perimeter of any one face. Each present also requires a bow made out of ribbon as well; the feet of ribbon required for the perfect bow is equal to the cubic feet of volume of the present. Don't ask how they tie the bow, though; they'll never tell.
@@ -63,6 +59,7 @@ For example:
 
 A present with dimensions 2x3x4 requires 2+2+3+3 = 10 feet of ribbon to wrap the present plus 2*3*4 = 24 feet of ribbon for the bow, for a total of 34 feet.
 A present with dimensions 1x1x10 requires 1+1+1+1 = 4 feet of ribbon to wrap the present plus 1*1*10 = 10 feet of ribbon for the bow, for a total of 14 feet.
+
 How many total feet of ribbon should they order?
  */
 // Your puzzle answer was
@@ -73,16 +70,12 @@ How many total feet of ribbon should they order?
 /// ```
 #[aoc(day2, part2)]
 pub fn solve_part_02(input: &[(u32, u32, u32)]) -> u32 {
-    let mut total_ribbon = 0;
-
-    for (length, width, height) in input {
+    input.iter().fold(0, |sum, (length, width, height)| {
         let mut all_lengths = [length, width, height];
         all_lengths.sort();
 
-        total_ribbon += 2 * all_lengths[0] + 2 * all_lengths[1] + (length * width * height)
-    }
-
-    total_ribbon
+        2 * all_lengths[0] + 2 * all_lengths[1] + (length * width * height) + sum
+    })
 }
 
 #[cfg(test)]
