503a6ed438f8a754a643da5396a6499a4777e35f
Rickard Natt och Dag <rickard@nattochdag.dev>
2021-12-01 06:39:52 +0100

    feat(2021): add day 1 part 2

--- rust/2021/src/day_01.rs
+++ rust/2021/src/day_01.rs
@@ -37,11 +37,24 @@ pub fn solve_part_01(input: &Vec<u32>) -> u32 {
 /// ```
 /// use advent_of_code_2021::day_01::*;
 /// let data = include_str!("../input/2021/day1.txt");
-/// assert_eq!(solve_part_02(&input_generator(data)), 1713);
+/// assert_eq!(solve_part_02(&input_generator(data)), 1734);
 /// ```
 #[aoc(day1, part2)]
-pub fn solve_part_02(_input: &Vec<u32>) -> u32 {
-    0
+pub fn solve_part_02(input: &Vec<u32>) -> u32 {
+    let mut last_sum = 0;
+    let mut increases = 0;
+
+    for i in input.windows(3) {
+        let new_sum = i.iter().sum();
+
+        if new_sum > last_sum && last_sum != 0 {
+            increases += 1;
+        }
+
+        last_sum = new_sum;
+    }
+
+    increases
 }
 
 #[cfg(test)]
@@ -62,5 +75,21 @@ mod tests {
 263";
 
         assert_eq!(solve_part_01(&input_generator(data)), 7)
+    }
+
+    #[test]
+    fn sample_02() {
+        let data = "199
+200
+208
+210
+200
+207
+240
+269
+260
+263";
+
+        assert_eq!(solve_part_02(&input_generator(data)), 5)
     }
 }
