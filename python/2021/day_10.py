0e605a18fe068332ea05fe084319ee8e17dec7ea
Rickard Natt och Dag <rickard@fastmail.se>
2021-12-10 07:32:30 +0100

    refactor: add is_corrupt method

--- python/2021/day_10.py
+++ python/2021/day_10.py
@@ -3,23 +3,35 @@ points_part_two = {'(': 1, '[': 2, '{': 3, '<': 4}
 open_brackets = ('(', '[', '{', '<')
 
 
+def is_corrupt(a, b):
+	return abs(ord(a) - ord(b)) > 3
+
+
+def find_open_brackets(line):
+	openers = []
+	score = 0
+
+	for c in line.strip():
+		if c in open_brackets:
+			openers.append(c)
+		else:
+			b = openers.pop()
+
+			# End on corrupted lines
+			if is_corrupt(b, c):
+				score += points[ord(c)]
+				break
+
+	return (score, openers)
+
+
 def part_01():
 	with open('input_10.txt') as d:
 		score = 0
 
 		for line in d:
-			openers = []
-
-			for c in line.strip():
-				if c in open_brackets:
-					openers.append(c)
-				else:
-					b = openers.pop()
-
-					# End on corrupted lines
-					if abs(ord(b) - ord(c)) > 3:
-						score += points[ord(c)]
-						break
+			(line_score, _) = find_open_brackets(line)
+			score += line_score
 
 		return score
 
@@ -39,7 +51,7 @@ def part_02():
 					b = openers.pop()
 
 					# End on corrupted lines
-					if abs(ord(b) - ord(c)) > 3:
+					if is_corrupt(b, c):
 						break
 			else:
 				# Remove the open brackets one by one and
