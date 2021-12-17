1cca36457350a4167a180ea6fdec61f8342088b4
Rickard Natt och Dag <rickard@fastmail.se>
2021-12-17 07:48:12 +0100

    feat(2021): add day 17 part 1

---
+++ python/2021/day_17.py
@@ -0,0 +1,40 @@
+with open('input.txt') as d:
+	lines = d.read().strip().split(': ')
+	target = lines[1].split(',')
+	target_x = list(map(int, target[0][2:].split('..')))
+	target_y = list(map(int, target[1][3:].split('..')))
+
+part_one = 0
+
+for dxx in range(max(target_x) + 1):
+	for dyy in range(min(target_y) - 1, 1000):
+		done = False
+		highest_y = 0
+		x = 0
+		y = 0
+		dx = dxx
+		dy = dyy
+
+		for t in range(1000):
+			x += dx
+			y += dy
+			highest_y = max(highest_y, y)
+
+			# Drag
+			if dx > 0:
+				dx -= 1
+			elif dx < 0:
+				dx += 1
+
+			# Gravity
+			dy -= 1
+
+			if target_x[0] <= x <= target_x[1] and target_y[0] <= y <= target_y[1]:
+				done = True
+
+		if done:
+			if highest_y > part_one:
+				part_one = highest_y
+				print('x:', dxx, 'y:', dyy, 'max height:', part_one)
+
+print('Part 1:', part_one)
