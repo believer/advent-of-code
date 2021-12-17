b2030740673d6f4f7e8ba43e8e2a82de0831795a
Rickard Natt och Dag <rickard@fastmail.se>
2021-12-17 08:09:25 +0100

    feat(2021): add day 17 part 2

--- python/2021/day_17.py
+++ python/2021/day_17.py
@@ -1,3 +1,5 @@
+# Slow part 2, at least when running it on an iPad, but it works.
+
 with open('input.txt') as d:
 	lines = d.read().strip().split(': ')
 	target = lines[1].split(',')
@@ -5,10 +7,11 @@ with open('input.txt') as d:
 	target_y = list(map(int, target[1][3:].split('..')))
 
 part_one = 0
+part_two = 0
 
 for dxx in range(max(target_x) + 1):
 	for dyy in range(min(target_y) - 1, 1000):
-		done = False
+		hit_target = False
 		highest_y = 0
 		x = 0
 		y = 0
@@ -30,11 +33,13 @@ for dxx in range(max(target_x) + 1):
 			dy -= 1
 
 			if target_x[0] <= x <= target_x[1] and target_y[0] <= y <= target_y[1]:
-				done = True
+				hit_target = True
 
-		if done:
+		if hit_target:
+			part_two += 1
 			if highest_y > part_one:
 				part_one = highest_y
 				print('x:', dxx, 'y:', dyy, 'max height:', part_one)
 
 print('Part 1:', part_one)
+print('Part 2:', part_two)
