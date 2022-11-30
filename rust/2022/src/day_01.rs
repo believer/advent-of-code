523778a590642bf5ba3fad7b90a7f6c68f789b73
Rickard Natt och Dag <rdag@invidi.com>
2022-11-30 21:19:54 +0100

    chore: preparing for 2022

---
+++ rust/2022/src/day_01.rs
@@ -0,0 +1,46 @@
+use crate::common;
+
+// Day 1:
+
+#[aoc_generator(day1)]
+pub fn input_generator(input: &str) -> Vec<u32> {
+    common::input_vec(input)
+}
+
+/* Part One
+*/
+// Your puzzle answer was
+/// ```
+/// use advent_of_code_2022::day_01::*;
+/// let data = include_str!("../input/2022/day1.txt");
+/// assert_eq!(solve_part_01(&input_generator(data)), 0);
+/// ```
+#[aoc(day1, part1)]
+pub fn solve_part_01(input: &Vec<u32>) -> u32 {
+    0
+}
+
+/* Part Two
+*/
+// Your puzzle answer was
+/// ```
+/// use advent_of_code_2022::day_01::*;
+/// let data = include_str!("../input/2022/day1.txt");
+/// assert_eq!(solve_part_02(&input_generator(data)), 0);
+/// ```
+#[aoc(day1, part2)]
+pub fn solve_part_02(input: &Vec<u32>) -> u32 {
+    0
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn sample_01() {
+        let data = "test";
+
+        assert_eq!(solve_part_01(&input_generator(data)), 0)
+    }
+}
---
+++ rust/2022/src/lib.rs
@@ -0,0 +1,11 @@
+extern crate aoc_runner;
+
+#[macro_use]
+extern crate aoc_runner_derive;
+
+pub mod common;
+pub mod math;
+
+pub mod day_01;
+
+aoc_lib! { year = 2022 }
