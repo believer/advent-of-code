5e2c5a4ecb71ad9a44a43749c3881c7dd4f13f14
Rickard Natt och Dag <rickard@nattochdag.dev>
2022-12-01 10:42:45 +0100

    feat(2022): add day 1

--- rust/2022/src/day_01.rs
+++ rust/2022/src/day_01.rs
@@ -1,10 +1,12 @@
-use crate::common;
+use std::{cmp, num::ParseIntError};
 
-// Day 1:
+// Day 1
+
+type Input = Vec<Result<u32, ParseIntError>>;
 
 #[aoc_generator(day1)]
-pub fn input_generator(input: &str) -> Vec<u32> {
-    common::input_vec(input)
+pub fn input_generator(input: &str) -> Input {
+    input.lines().map(|l| l.trim()).map(|l| l.parse()).collect()
 }
 
 /* Part One
@@ -13,11 +15,26 @@ pub fn input_generator(input: &str) -> Vec<u32> {
 /// ```
 /// use advent_of_code_2022::day_01::*;
 /// let data = include_str!("../input/2022/day1.txt");
-/// assert_eq!(solve_part_01(&input_generator(data)), 0);
+/// assert_eq!(solve_part_01(&input_generator(data)), 69528);
 /// ```
 #[aoc(day1, part1)]
-pub fn solve_part_01(_input: &[u32]) -> u32 {
-    0
+pub fn solve_part_01(input: &Input) -> u32 {
+    let mut max: u32 = 0;
+    let mut current: u32 = 0;
+
+    for i in input {
+        match i {
+            Ok(v) => {
+                current += v;
+            }
+            Err(_) => {
+                max = cmp::max(max, current);
+                current = 0;
+            }
+        }
+    }
+
+    max
 }
 
 /* Part Two
@@ -26,11 +43,36 @@ pub fn solve_part_01(_input: &[u32]) -> u32 {
 /// ```
 /// use advent_of_code_2022::day_01::*;
 /// let data = include_str!("../input/2022/day1.txt");
-/// assert_eq!(solve_part_02(&input_generator(data)), 0);
+/// assert_eq!(solve_part_02(&input_generator(data)), 206152);
 /// ```
 #[aoc(day1, part2)]
-pub fn solve_part_02(_input: &[u32]) -> u32 {
-    0
+pub fn solve_part_02(input: &Input) -> u32 {
+    let mut all = vec![];
+    let mut current: u32 = 0;
+    let mut iter = 0;
+
+    for i in input {
+        iter += 1;
+
+        match i {
+            Ok(v) => {
+                current += v;
+
+                // Handle last value
+                if iter == input.len() {
+                    all.push(current);
+                }
+            }
+            Err(_) => {
+                all.push(current);
+                current = 0;
+            }
+        }
+    }
+
+    all.sort();
+    // Convert to an iterator, reverse it, get the first three values, and sum them
+    all.iter().rev().take(3).sum::<u32>()
 }
 
 #[cfg(test)]
@@ -39,8 +81,41 @@ mod tests {
 
     #[test]
     fn sample_01() {
-        let data = "test";
+        let data = "1000
+2000
+3000
 
-        assert_eq!(solve_part_01(&input_generator(data)), 0)
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000";
+
+        assert_eq!(solve_part_01(&input_generator(data)), 24000)
+    }
+
+    #[test]
+    fn sample_02() {
+        let data = "1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000";
+
+        assert_eq!(solve_part_02(&input_generator(data)), 45000)
     }
 }
