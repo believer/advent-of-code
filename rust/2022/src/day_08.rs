c87848a33a35ac738c442a3158903c6a18841bf4
Rickard Natt och Dag <rickard@nattochdag.dev>
2022-12-08 16:06:21 +0100

    refactor(2022): clean up and documentation for day 8

--- rust/2022/src/day_08.rs
+++ rust/2022/src/day_08.rs
@@ -17,7 +17,20 @@ pub fn input_generator(input: &str) -> Input {
         .collect()
 }
 
+fn find_inner_trees(input: &Input) -> Vec<Vec<&u32>> {
+    input[1..input.len() - 1]
+        .iter()
+        .map(|row| row[1..row.len() - 1].iter().collect())
+        .collect::<Vec<Vec<_>>>()
+}
+
 /* Part One
+ *
+ * In this part, we need to find the number of trees that are visible.
+ * A visible tree is defined as a tree that is not blocked by a taller tree in
+ * at least on direction.
+ *
+ * The edges of the forest are always visible, so we can count them first.
 */
 /// Your puzzle answer was
 /// ```
@@ -27,38 +40,34 @@ pub fn input_generator(input: &str) -> Input {
 /// ```
 #[aoc(day8, part1)]
 pub fn solve_part_01(input: &Input) -> u32 {
+    // We know that the outer most ring of tree are always visible.
+    // That's the length of the first and last row plus
+    // the length of the first and last column (without the first
+    // and the last row).
     let outer_rows = input.get(0).unwrap().len() * 2;
     let outer_cols = (input.len() - 2) * 2;
 
     let mut visible = (outer_rows + outer_cols) as u32;
 
-    let inner_rows = input[1..input.len() - 1].iter();
-    let inner = inner_rows
-        .clone()
-        .map(|row| row[1..row.len() - 1].iter().collect())
-        .collect::<Vec<Vec<_>>>();
-
-    for (i, row) in inner.iter().enumerate().map(|(i, row)| (i + 1, row)) {
+    // Here we create a new vector that has removed the outermost
+    // ring of trees as they have already been counted.
+    for (i, row) in find_inner_trees(input)
+        .iter()
+        .enumerate()
+        .map(|(i, row)| (i + 1, row))
+    {
         for (j, col) in row.iter().enumerate().map(|(j, col)| (j + 1, col)) {
-            let value = **col;
+            let tree_size = **col;
 
-            let all_bottom = (i..=input.len()).all(|v| {
-                if (v + 1) >= input.len() {
-                    return true;
-                }
+            // Find if there are taller tree in all directions
+            // If _one_ direction only has smaller trees to the edge
+            // the tree is visible.
+            let all_bottom = (i..input.len() - 1).all(|v| tree_size > input[v + 1][j]);
+            let all_top = (1..=i).all(|v| tree_size > input[v - 1][j]);
+            let all_left = (1..=j).all(|v| tree_size > input[i][v - 1]);
+            let all_right = (j..=row.len()).all(|v| tree_size > input[i][v + 1]);
 
-                let bottom = input[v + 1][j];
-
-                value > bottom
-            });
-
-            let all_top = (1..=i).all(|v| value > input[v - 1][j]);
-            let all_left = (1..=j).all(|v| value > input[i][v - 1]);
-            let all_right = (j..=row.len()).all(|v| value > input[i][v + 1]);
-
-            let tree_is_visible = all_top || all_bottom || all_left || all_right;
-
-            if tree_is_visible {
+            if all_top || all_bottom || all_left || all_right {
                 visible += 1;
             }
         }
@@ -68,6 +77,20 @@ pub fn solve_part_01(input: &Input) -> u32 {
 }
 
 /* Part Two
+ *
+ * In this part, we need to find the tree that has the largest scenic score.
+ * The scenic score is defined as the number of trees that are visible from
+ * the tree. That's counting _from_ the tree outwards until we hit a tree of
+ * the same size or taller. The taller tree is counted, the tree itself is not counted.
+ *
+ * For example:
+ *
+ * - We see 3 upwards
+ * - We see 2 downwards
+ * - We see 1 left
+ * - We see 2 right
+ *
+ * The scenic score is 3 * 2 * 1 * 2 = 12
 */
 /// Your puzzle answer was
 /// ```
@@ -79,67 +102,67 @@ pub fn solve_part_01(input: &Input) -> u32 {
 pub fn solve_part_02(input: &Input) -> u32 {
     let mut scenic_scores: Vec<u32> = vec![];
 
-    let inner_rows = input[1..input.len() - 1].iter();
-    let inner = inner_rows
-        .clone()
-        .map(|row| row[1..row.len() - 1].iter().collect())
-        .collect::<Vec<Vec<_>>>();
-
-    for (i, row) in inner.iter().enumerate().map(|(i, row)| (i + 1, row)) {
+    // Again we only need to check the inner trees
+    // This tripped me up for a while since I still had the ranges from
+    // before. Some of them needed to be reversed to look outwards.
+    for (i, row) in find_inner_trees(input)
+        .iter()
+        .enumerate()
+        .map(|(i, row)| (i + 1, row))
+    {
         for (j, col) in row.iter().enumerate().map(|(j, col)| (j + 1, col)) {
-            let value = **col;
+            let tree_size = **col;
             let mut top_score = 0;
             let mut right_score = 0;
             let mut bottom_score = 0;
             let mut left_score = 0;
 
+            // Look upwards
             for v in (1..=i).rev() {
-                let top = input[v - 1][j];
-
                 top_score += 1;
 
-                if top >= value {
+                if input[v - 1][j] >= tree_size {
                     break;
                 }
             }
 
+            // Look downwards
             for v in i..=input.len() {
                 if (v + 1) >= input.len() {
                     break;
                 }
-                let bottom = input[v + 1][j];
 
                 bottom_score += 1;
 
-                if bottom >= value {
+                if input[v + 1][j] >= tree_size {
                     break;
                 }
             }
 
+            // Look left
             for v in (1..=j).rev() {
-                let left = input[i][v - 1];
-
                 left_score += 1;
 
-                if left >= value {
+                if input[i][v - 1] >= tree_size {
                     break;
                 }
             }
 
+            // Look right
             for v in j..=row.len() {
-                let right = input[i][v + 1];
-
                 right_score += 1;
 
-                if right >= value {
+                if input[i][v + 1] >= tree_size {
                     break;
                 }
             }
 
+            // Add the score to the list
             scenic_scores.push(top_score * right_score * bottom_score * left_score);
         }
     }
 
+    // Find the largest scenic score
     *scenic_scores.iter().max().unwrap()
 }
 
