25f0d99e054a82223a2a638947a3c0802cb89454
Rickard Natt och Dag <rickard@nattochdag.dev>
2022-12-10 05:13:49 +0100

    chore: comment out tests for rust 2022 day 10

--- rust/2022/src/day_10.rs
+++ rust/2022/src/day_10.rs
@@ -11,26 +11,26 @@ pub fn input_generator(input: &str) -> Input {
 
 /* Part One
 */
-/// Your puzzle answer was
-/// ```
-/// use advent_of_code_2022::day_10::*;
-/// let data = include_str!("../input/2022/day10.txt");
-/// assert_eq!(solve_part_01(&input_generator(data)), 0);
-/// ```
+// /// Your puzzle answer was
+// /// ```
+// /// use advent_of_code_2022::day_10::*;
+// /// let data = include_str!("../input/2022/day10.txt");
+// /// assert_eq!(solve_part_01(&input_generator(data)), 0);
+// /// ```
 #[aoc(day10, part1)]
 pub fn solve_part_01(input: &Input) -> u32 {
     println!("{:?}", input);
-    1
+    0
 }
 
 /* Part Two
 */
-/// Your puzzle answer was
-/// ```
-/// use advent_of_code_2022::day_10::*;
-/// let data = include_str!("../input/2022/day10.txt");
-/// assert_eq!(solve_part_02(&input_generator(data)), 0);
-/// ```
+// /// Your puzzle answer was
+// /// ```
+// /// use advent_of_code_2022::day_10::*;
+// /// let data = include_str!("../input/2022/day10.txt");
+// /// assert_eq!(solve_part_02(&input_generator(data)), 0);
+// /// ```
 #[aoc(day10, part2)]
 pub fn solve_part_02(_input: &Input) -> u32 {
     0
