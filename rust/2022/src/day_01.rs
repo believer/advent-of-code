b25f9d3aa9688a56ff928f4d6f5eaff989d41ecb
Rickard Natt och Dag <rickard@nattochdag.dev>
2022-12-12 15:34:29 +0100

    docs(2022): write own description for day 1

--- rust/2022/src/day_01.rs
+++ rust/2022/src/day_01.rs
@@ -39,19 +39,8 @@ pub fn input_generator(input: &str) -> CaloriesPerElf {
 
 /* Part One
  *
- * The jungle must be too overgrown and difficult to navigate in
- * vehicles or access from the air; the Elves' expedition traditionally
- * goes on foot. As your boats approach land, the Elves begin taking
- * inventory of their supplies. One important consideration is
- * food - in particular, the number of Calories each Elf is carrying (your puzzle input).
- *
- * The Elves take turns writing down the number of Calories contained
- * by the various meals, snacks, rations, etc. that they've brought with
- * them, one item per line. Each Elf separates their own inventory from
- * the previous Elf's inventory (if any) by a blank line.
- *
- * For example, suppose the Elves finish writing their items'
- * Calories and end up with the following list:
+ * We get a list of elves, separated by a double line-break. Each elf carries a
+ * couple of items and each line corresponds to that item's calories. For example:
  *
  * 1000
  * 2000
@@ -62,25 +51,11 @@ pub fn input_generator(input: &str) -> CaloriesPerElf {
  * 5000
  * 6000
  *
- * 7000
- * 8000
- * 9000
+ * We want to find the elf that is carrying the most calories. Sum up the calories
+ * for each elf and find the largest number.
  *
- * 10000
- * This list represents the Calories of the food carried by five Elves:
- *
- * The first Elf is carrying food with 1000, 2000, and 3000 Calories, a total of 6000 Calories.
- * The second Elf is carrying one food item with 4000 Calories.
- * The third Elf is carrying food with 5000 and 6000 Calories, a total of 11000 Calories.
- * The fourth Elf is carrying food with 7000, 8000, and 9000 Calories, a total of 24000 Calories.
- * The fifth Elf is carrying one food item with 10000 Calories.
- *
- * In case the Elves get hungry and need extra snacks, they need to
- * know which Elf to ask: they'd like to know how many Calories are
- * being carried by the Elf carrying the most Calories. In the example
- * above, this is 24000 (carried by the fourth Elf).
- *
- * Find the Elf carrying the most Calories. How many total Calories is that Elf carrying?
+ * From the example above, the calories for each elf are 6000, 4000, 11000. So, 11000
+ * would be our answer.
 */
 /// Your puzzle answer was
 /// ```
@@ -90,27 +65,13 @@ pub fn input_generator(input: &str) -> CaloriesPerElf {
 /// ```
 #[aoc(day1, part1)]
 pub fn solve_part_01(elves: &CaloriesPerElf) -> u32 {
-    // Get the Elf carrying the most calories of snacks
     *elves.first().unwrap()
 }
 
 /* Part Two
  *
- * By the time you calculate the answer to the Elves' question,
- * they've already realized that the Elf carrying the most Calories
- * of food might eventually run out of snacks.
- *
- * To avoid this unacceptable situation, the Elves would instead
- * like to know the total Calories carried by the top three Elves
- * carrying the most Calories. That way, even if one of those Elves
- * runs out of snacks, they still have two backups.
- *
- * In the example above, the top three Elves are the fourth Elf (with 24000 Calories),
- * then the third Elf (with 11000 Calories), then the fifth Elf (with 10000 Calories).
- * The sum of the Calories carried by these three elves is 45000.
- *
- * Find the top three Elves carrying the most Calories.
- * How many Calories are those Elves carrying in total?
+ * In part two we want to find the top three elves and sum up the
+ * amount of calories they are carrying.
 */
 /// Your puzzle answer was
 /// ```
@@ -120,7 +81,6 @@ pub fn solve_part_01(elves: &CaloriesPerElf) -> u32 {
 /// ```
 #[aoc(day1, part2)]
 pub fn solve_part_02(elves: &CaloriesPerElf) -> u32 {
-    // Get the amount of calories carried by the top three Elves and sum it
     elves.iter().take(3).sum::<u32>()
 }
 
