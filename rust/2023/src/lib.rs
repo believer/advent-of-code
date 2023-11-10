8adb06b88d4701762d9e4ff4c4f51952ff85b3cb
believer <rickard@nattochdag.dev>
2023-11-10 16:15:10 +0100

    chore(2023): prepping for aoc 2023

---
+++ rust/2023/src/lib.rs
@@ -0,0 +1,13 @@
+#![feature(iter_array_chunks)]
+
+extern crate aoc_runner;
+
+#[macro_use]
+extern crate aoc_runner_derive;
+
+pub mod common;
+pub mod math;
+
+pub mod day_01;
+
+aoc_lib! { year = 2023 }
