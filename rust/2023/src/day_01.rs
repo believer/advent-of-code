f612ca94ada90d3f4c9765f667d6c941d1bc2532
believer <rickard@nattochdag.dev>
2023-11-10 16:19:07 +0100

    chore: run tests on 2023

--- rust/2023/src/day_01.rs
+++ rust/2023/src/day_01.rs
@@ -10,11 +10,13 @@ pub fn input_generator(input: &str) -> Vec<u32> {
 /* Part One
 */
 // Your puzzle answer was
+/* DOCTEST DISABLED
 /// ```
-/// use advent_of_code_2022::day_01::*;
+/// use advent_of_code_2023::day_01::*;
 /// let data = include_str!("../input/2022/day1.txt");
 /// assert_eq!(solve_part_01(&input_generator(data)), 0);
 /// ```
+*/
 #[aoc(day1, part1)]
 pub fn solve_part_01(_input: &Vec<u32>) -> u32 {
     0
@@ -23,11 +25,13 @@ pub fn solve_part_01(_input: &Vec<u32>) -> u32 {
 /* Part Two
 */
 // Your puzzle answer was
+/* DOCTEST DISABLED
 /// ```
-/// use advent_of_code_2022::day_01::*;
+/// use advent_of_code_2023::day_01::*;
 /// let data = include_str!("../input/2022/day1.txt");
 /// assert_eq!(solve_part_02(&input_generator(data)), 0);
 /// ```
+*/
 #[aoc(day1, part2)]
 pub fn solve_part_02(_input: &Vec<u32>) -> u32 {
     0
@@ -39,7 +43,7 @@ mod tests {
 
     #[test]
     fn sample_01() {
-        let data = "test";
+        let data = "0";
 
         assert_eq!(solve_part_01(&input_generator(data)), 0)
     }
