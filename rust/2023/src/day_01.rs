a773b43a0cc928fc068354387c9cadba4a0b569c
believer <rickard@nattochdag.dev>
2023-12-01 08:57:51 +0100

    feat(2023): clean up day 1

--- rust/2023/src/day_01.rs
+++ rust/2023/src/day_01.rs
@@ -1,5 +1,3 @@
-use regex::Regex;
-
 // Day 1: Trebuchet?!
 //
 // The first day of the Advent of Code 2023 is here!
@@ -7,71 +5,9 @@ use regex::Regex;
 // was a bit more tricky. I couldn't find a way to do it in code,
 // so I massaged the data in vim.
 
-#[aoc_generator(day1, part1)]
-pub fn input_generator_part1(input: &str) -> Vec<u32> {
-    input
-        .lines()
-        .map(|l| {
-            l.chars()
-                .filter(|c| c.is_ascii_digit())
-                .collect::<String>()
-                .parse::<u32>()
-                .unwrap()
-        })
-        .map(|n| {
-            // First and last digits of the number
-            let t = n.to_string();
-            let i = t.chars().next().unwrap();
-            let i2 = t.chars().next_back().unwrap();
-
-            i.to_string() + &i2.to_string()
-        })
-        .map(|n| n.parse::<u32>().unwrap())
-        .collect()
-}
-
-#[aoc_generator(day1, part2)]
-pub fn input_generator_part2(input: &str) -> Vec<u32> {
-    let mut data: Vec<u32> = vec![];
-
-    for line in input.lines() {
-        let re = Regex::new(r"(one|two|three|four|five|six|seven|eight|nine)").unwrap();
-        let cleaned_data = re.replace_all(line, |caps: &regex::Captures| match &caps[0] {
-            "one" => "1",
-            "two" => "2",
-            "three" => "3",
-            "four" => "4",
-            "five" => "5",
-            "six" => "6",
-            "seven" => "7",
-            "eight" => "8",
-            "nine" => "9",
-            _ => unreachable!(),
-        });
-
-        let filtered_data = cleaned_data
-            .split_whitespace()
-            .map(|l| {
-                l.chars()
-                    .filter(|c| c.is_ascii_digit())
-                    .collect::<String>()
-                    .parse::<u32>()
-                    .unwrap()
-            })
-            .map(|n| {
-                // First and last digits of the number
-                let t = n.to_string();
-                let i = t.chars().next().unwrap();
-                let i2 = t.chars().next_back().unwrap();
-                i.to_string() + &i2.to_string()
-            })
-            .map(|n| n.parse::<u32>().unwrap())
-            .collect::<Vec<_>>()[0];
-
-        data.push(filtered_data);
-    }
-
-    data
+#[aoc_generator(day1)]
+pub fn input_generator(input: &str) -> String {
+    input.to_string()
 }
 
 /* Part One
@@ -95,11 +31,43 @@ pub fn input_generator_part2(input: &str) -> Vec<u32> {
 /// ```
 /// use advent_of_code_2023::day_01::*;
 /// let data = include_str!("../input/2023/day1.txt");
-/// assert_eq!(solve_part_01(&input_generator_part1(data)), 54304);
+/// assert_eq!(solve_part_01(&input_generator(data)), 54304);
 /// ```
 #[aoc(day1, part1)]
-pub fn solve_part_01(input: &[u32]) -> u32 {
-    input.iter().sum()
+pub fn solve_part_01(input: &str) -> u32 {
+    let mut sum = 0;
+
+    for line in input.lines() {
+        let mut digits = line.chars().filter_map(|c| c.to_digit(10));
+
+        let first_digit = digits.next().unwrap();
+        let last_digit = digits.last().unwrap_or(first_digit);
+
+        sum += first_digit * 10 + last_digit;
+    }
+
+    sum
+}
+
+#[rustfmt::skip]
+const DIGITS_AS_TEXT: [&str; 18] = [
+    "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+    "1", "2", "3", "4", "5", "6", "7", "8", "9",
+];
+
+fn digit_to_number(digit: &str) -> u32 {
+    match digit {
+        "one" => 1,
+        "two" => 2,
+        "three" => 3,
+        "four" => 4,
+        "five" => 5,
+        "six" => 6,
+        "seven" => 7,
+        "eight" => 8,
+        "nine" => 9,
+        n => n.parse::<u32>().unwrap(),
+    }
 }
 
 /* Part Two
@@ -122,16 +90,48 @@ pub fn solve_part_01(input: &[u32]) -> u32 {
 *
 */
 // Your puzzle answer was
-/* DOCTEST DISABLED
 /// ```
 /// use advent_of_code_2023::day_01::*;
 /// let data = include_str!("../input/2023/day1.txt");
-/// assert_eq!(solve_part_02(&input_generator_part2(data)), 54418);
+/// assert_eq!(solve_part_02(&input_generator(data)), 54418);
 /// ```
-*/
 #[aoc(day1, part2)]
-pub fn solve_part_02(input: &[u32]) -> u32 {
-    input.iter().sum()
+pub fn solve_part_02(input: &str) -> u32 {
+    let mut sum = 0;
+
+    for line in input.lines() {
+        let mut first_position = None;
+        let mut first_digit = None;
+        let mut last_position = None;
+        let mut last_digit = None;
+
+        // We only need the first and the last digit
+        // so we can look from the front and the back to find them.
+        for digit in DIGITS_AS_TEXT {
+            // Find first digit
+            if let Some(position) = line.find(digit) {
+                if position <= first_position.unwrap_or(position) {
+                    first_position = Some(position);
+                    first_digit = Some(digit);
+                }
+            }
+
+            // Find last digit
+            if let Some(position) = line.rfind(digit) {
+                if position >= last_position.unwrap_or(position) {
+                    last_position = Some(position);
+                    last_digit = Some(digit);
+                }
+            }
+        }
+
+        let first = first_digit.map(digit_to_number).unwrap();
+        let last = last_digit.map(digit_to_number).unwrap();
+
+        sum += first * 10 + last;
+    }
+
+    sum
 }
 
 #[cfg(test)]
@@ -145,7 +145,7 @@ pqr3stu8vwx
 a1b2c3d4e5f
 treb7uchet";
 
-        assert_eq!(solve_part_01(&input_generator_part1(data)), 142)
+        assert_eq!(solve_part_01(&input_generator(data)), 142)
     }
 
     #[test]
@@ -158,13 +158,13 @@ xtwone3four
 zoneight234
 7pqrstsixteen";
 
-        assert_eq!(solve_part_02(&input_generator_part2(data)), 281)
+        assert_eq!(solve_part_02(&input_generator(data)), 281)
     }
 
     #[test]
     fn handle_overlap() {
         let data = "dleightwolvbvmsggs9njseven5fivethreenine";
 
-        assert_eq!(solve_part_02(&input_generator_part2(data)), 89)
+        assert_eq!(solve_part_02(&input_generator(data)), 89)
     }
 }
