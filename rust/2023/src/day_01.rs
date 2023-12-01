c4a4213b4a9d9c320e43c198916dc0f1d89e57c5
believer <rickard@nattochdag.dev>
2023-12-01 07:24:35 +0100

    feat(2023): add day 1

--- rust/2023/src/day_01.rs
+++ rust/2023/src/day_01.rs
@@ -1,40 +1,138 @@
-use crate::common;
+use regex::Regex;
 
-// Day 1:
+// Day 1: Trebuchet?!
 
-#[aoc_generator(day1)]
-pub fn input_generator(input: &str) -> Vec<u32> {
-    common::input_vec(input)
+#[aoc_generator(day1, part1)]
+pub fn input_generator_part1(input: &str) -> Vec<u32> {
+    input
+        .lines()
+        .map(|l| {
+            l.chars()
+                .filter(|c| c.is_digit(10))
+                .collect::<String>()
+                .parse::<u32>()
+                .unwrap()
+        })
+        .map(|n| {
+            // First and last digits of the number
+            let t = n.to_string();
+            let i = t.chars().next().unwrap();
+            let i2 = t.chars().next_back().unwrap();
+
+            let string = i.to_string() + &i2.to_string();
+
+            return string;
+        })
+        .map(|n| n.parse::<u32>().unwrap())
+        .collect()
+}
+
+#[aoc_generator(day1, part2)]
+pub fn input_generator_part2(input: &str) -> Vec<u32> {
+    let mut data: Vec<u32> = vec![];
+
+    for line in input.lines() {
+        let re = Regex::new(r"(one|two|three|four|five|six|seven|eight|nine)").unwrap();
+        let cleaned_data = re.replace_all(line, |caps: &regex::Captures| match &caps[0] {
+            "one" => "1",
+            "two" => "2",
+            "three" => "3",
+            "four" => "4",
+            "five" => "5",
+            "six" => "6",
+            "seven" => "7",
+            "eight" => "8",
+            "nine" => "9",
+            _ => unreachable!(),
+        });
+
+        let filtered_data = cleaned_data
+            .split_whitespace()
+            .map(|l| {
+                l.chars()
+                    .filter(|c| c.is_digit(10))
+                    .collect::<String>()
+                    .parse::<u32>()
+                    .unwrap()
+            })
+            .map(|n| {
+                // First and last digits of the number
+                let t = n.to_string();
+                let i = t.chars().next().unwrap();
+                let i2 = t.chars().next_back().unwrap();
+                let string = i.to_string() + &i2.to_string();
+
+                return string;
+            })
+            .map(|n| n.parse::<u32>().unwrap())
+            .collect::<Vec<_>>()[0];
+
+        data.push(filtered_data);
+    }
+
+    return data;
 }
 
 /* Part One
+*
+* We are given a list of numbers mixed with letters. We need to extract the FIRST and LAST number of
+* each line as an integer, and sum them up.
+*
+* Example:
+*
+* 1abc2
+* pqr3stu8vwx
+* a1b2c3d4e5f
+* treb7uchet
+*
+* Or, 12 + 38 + 15 + 77 = 82
+*
+* Note that the last line only has one number, so it is repeated.
+*
 */
 // Your puzzle answer was
-/* DOCTEST DISABLED
 /// ```
 /// use advent_of_code_2023::day_01::*;
-/// let data = include_str!("../input/2022/day1.txt");
-/// assert_eq!(solve_part_01(&input_generator(data)), 0);
+/// let data = include_str!("../input/2023/day1.txt");
+/// assert_eq!(solve_part_01(&input_generator_part1(data)), 54304);
 /// ```
-*/
 #[aoc(day1, part1)]
-pub fn solve_part_01(_input: &[u32]) -> u32 {
-    0
+pub fn solve_part_01(input: &[u32]) -> u32 {
+    input.iter().sum()
 }
 
 /* Part Two
+*
+* In part two we find out that the strings can contain words that
+* are numbers. One to nine needs to be replaced with the actual number.
+*
+* Example:
+* two1nine
+* eightwothree
+* abcone2threexyz
+* xtwone3four
+* 4nineeightseven2
+* zoneight234
+* 7pqrstsixteen
+*
+* 29, 83, 13, 24, 42, 14, and 76
+*
+* Some of the numbers overlap, like eightwothree should be 823 when cleaned.
+*
+* I couldn't find an easy way of doing this off the top of my head, so I
+* massaged the data manually in vim by replacing the words with the numbers.
 */
 // Your puzzle answer was
 /* DOCTEST DISABLED
 /// ```
 /// use advent_of_code_2023::day_01::*;
-/// let data = include_str!("../input/2022/day1.txt");
-/// assert_eq!(solve_part_02(&input_generator(data)), 0);
+/// let data = include_str!("../input/2023/day1.txt");
+/// assert_eq!(solve_part_02(&input_generator_part2(data)), 54418);
 /// ```
 */
 #[aoc(day1, part2)]
-pub fn solve_part_02(_input: &[u32]) -> u32 {
-    0
+pub fn solve_part_02(input: &[u32]) -> u32 {
+    input.iter().sum()
 }
 
 #[cfg(test)]
@@ -43,8 +141,31 @@ mod tests {
 
     #[test]
     fn sample_01() {
-        let data = "0";
+        let data = "1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet";
 
-        assert_eq!(solve_part_01(&input_generator(data)), 0)
+        assert_eq!(solve_part_01(&input_generator_part1(data)), 142)
+    }
+
+    #[test]
+    fn sample_02() {
+        let data = "two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen";
+
+        assert_eq!(solve_part_02(&input_generator_part2(data)), 281)
+    }
+
+    #[test]
+    fn handle_overlap() {
+        let data = "dleightwolvbvmsggs9njseven5fivethreenine";
+
+        assert_eq!(solve_part_02(&input_generator_part2(data)), 89)
     }
 }
