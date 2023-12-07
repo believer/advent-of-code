ea064bad8f1ce6b982c68f43fb4a1e629bba0f97
believer <rickard@nattochdag.dev>
2023-12-07 07:05:26 +0100

    feat(2023): add day 7 part 1

--- rust/2023/src/day_07.rs
+++ rust/2023/src/day_07.rs
@@ -1,12 +1,91 @@
+use std::collections::BTreeMap;
+
 // Day 7:
 
+#[derive(Debug, Eq, PartialEq, Clone, Hash, Ord, PartialOrd)]
+enum Card {
+    Number(u8),
+    Ten,
+    Jack,
+    Queen,
+    King,
+    Ace,
+}
+
+#[derive(Debug, Eq, PartialEq, Clone, Ord, PartialOrd)]
+enum HandType {
+    HighCard,
+    OnePair,
+    TwoPairs,
+    ThreeOfAKind,
+    FullHouse,
+    FourOfAKind,
+    FiveOfAKind,
+}
+
+#[derive(Debug, Eq, PartialEq, Clone)]
+pub struct Hand {
+    hand_type: HandType,
+    cards: Vec<Card>,
+    bid: u64,
+}
+
+impl From<&str> for Hand {
+    fn from(s: &str) -> Self {
+        let mut parts = s.split_whitespace();
+        let hand = parts.next().unwrap().to_string();
+        let bid = parts.next().unwrap().parse::<u64>().unwrap();
+
+        let cards = hand
+            .chars()
+            .map(|c| match c {
+                'T' => Card::Ten,
+                'J' => Card::Jack,
+                'Q' => Card::Queen,
+                'K' => Card::King,
+                'A' => Card::Ace,
+                _ => Card::Number(c.to_digit(10).unwrap() as u8),
+            })
+            .collect::<Vec<_>>();
+
+        let mut cards_by_type: BTreeMap<Card, u8> = BTreeMap::new();
+
+        for card in cards.clone() {
+            let c = cards_by_type.entry(card).or_insert(0);
+            *c += 1;
+        }
+
+        let cc = cards_by_type.values().collect::<Vec<_>>();
+
+        let hand_type = match &cc[..] {
+            [1, 1, 1, 1, 1] => HandType::HighCard,
+            [1, 1, 1, 2] | [1, 1, 2, 1] | [1, 2, 1, 1] | [2, 1, 1, 1] => HandType::OnePair,
+            [1, 2, 2] | [2, 1, 2] | [2, 2, 1] => HandType::TwoPairs,
+            [1, 1, 3] | [1, 3, 1] | [3, 1, 1] => HandType::ThreeOfAKind,
+            [2, 3] | [3, 2] => HandType::FullHouse,
+            [1, 4] | [4, 1] => HandType::FourOfAKind,
+            [5] => HandType::FiveOfAKind,
+            _ => panic!("Unknown hand type"),
+        };
+
+        Self {
+            cards,
+            bid,
+            hand_type,
+        }
+    }
+}
+
 #[derive(Debug)]
-pub struct Input {}
+pub struct Input {
+    hands: Vec<Hand>,
+}
 
 #[aoc_generator(day7)]
 pub fn input_generator(input: &str) -> Input {
-    println!("{input}");
-    Input {}
+    let hands = input.lines().map(|line| line.into()).collect();
+
+    Input { hands }
 }
 
 /* Part One
@@ -21,8 +100,32 @@ pub fn input_generator(input: &str) -> Input {
 /// ```
 */
 #[aoc(day7, part1)]
-pub fn solve_part_01(_input: &Input) -> u64 {
-    0
+pub fn solve_part_01(input: &Input) -> u64 {
+    let mut ranked: Vec<Hand> = Vec::with_capacity(input.hands.len());
+    let mut by_rank: BTreeMap<HandType, Vec<Hand>> = BTreeMap::new();
+    let mut sum = 0;
+
+    for hand in &input.hands {
+        let c = by_rank.entry(hand.hand_type.clone()).or_default();
+        c.push(hand.clone());
+    }
+
+    for (_, hands) in by_rank.iter() {
+        let mut hands = hands.clone();
+
+        hands.sort_by(|a, b| match (a.cards.clone(), b.cards.clone()) {
+            (a, b) if a == b => a.cmp(&b),
+            (a, b) => a.cmp(&b),
+        });
+
+        ranked.append(&mut hands);
+    }
+
+    for (i, hand) in ranked.iter().enumerate() {
+        sum += hand.bid * (i + 1) as u64;
+    }
+
+    sum
 }
 
 /* Part Two
@@ -48,10 +151,14 @@ pub fn solve_part_02(_input: &Input) -> u64 {
 mod tests {
     use super::*;
 
-    const DATA: &str = "";
+    const DATA: &str = "32T3K 765
+T55J5 684
+KK677 28
+KTJJT 220
+QQQJA 483";
 
     #[test]
     fn sample_01() {
-        assert_eq!(solve_part_01(&input_generator(DATA)), 0)
+        assert_eq!(solve_part_01(&input_generator(DATA)), 6440)
     }
 }
