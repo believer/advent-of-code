a6330bc1ae09b3f2b529548c2bc6654010824852
believer <rickard@nattochdag.dev>
2023-12-07 09:40:35 +0100

    chore(2023): day 7 even messier to fix tests

--- rust/2023/src/day_07.rs
+++ rust/2023/src/day_07.rs
@@ -11,24 +11,34 @@ pub struct Input {
 
 #[derive(Debug, Eq, PartialEq, Clone, Hash, Ord, PartialOrd)]
 enum Card {
+    Joker,
     Number(u8),
     Ten,
-    JackJoker,
+    Jack,
     Queen,
     King,
     Ace,
 }
 
-impl From<char> for Card {
-    fn from(c: char) -> Self {
-        match c {
-            'T' => Self::Ten,
-            'J' => Self::JackJoker,
-            'Q' => Self::Queen,
-            'K' => Self::King,
-            'A' => Self::Ace,
-            _ => Self::Number(c.to_digit(10).unwrap() as u8),
-        }
+fn parse_card(c: char) -> Card {
+    match c {
+        'T' => Card::Ten,
+        'J' => Card::Jack,
+        'Q' => Card::Queen,
+        'K' => Card::King,
+        'A' => Card::Ace,
+        _ => Card::Number(c.to_digit(10).unwrap() as u8),
+    }
+}
+
+fn parse_card2(c: char) -> Card {
+    match c {
+        'T' => Card::Ten,
+        'J' => Card::Joker,
+        'Q' => Card::Queen,
+        'K' => Card::King,
+        'A' => Card::Ace,
+        _ => Card::Number(c.to_digit(10).unwrap() as u8),
     }
 }
 
@@ -50,11 +60,11 @@ pub struct Hand {
     bid: u64,
 }
 
-fn parse_card(s: &str) -> Hand {
+fn parse_hand(s: &str) -> Hand {
     let mut parts = s.split_whitespace();
     let hand = parts.next().unwrap().to_string();
     let bid = parts.next().unwrap().parse::<u64>().unwrap();
-    let cards = hand.chars().map(|c| c.into()).collect::<Vec<Card>>();
+    let cards = hand.chars().map(parse_card).collect::<Vec<Card>>();
     let mut cards_by_type: BTreeMap<Card, u8> = BTreeMap::new();
 
     for card in cards.clone() {
@@ -82,11 +92,11 @@ fn parse_card(s: &str) -> Hand {
     }
 }
 
-fn parse_card2(s: &str) -> Hand {
+fn parse_hand2(s: &str) -> Hand {
     let mut parts = s.split_whitespace();
     let hand = parts.next().unwrap().to_string();
     let bid = parts.next().unwrap().parse::<u64>().unwrap();
-    let cards = hand.chars().map(|c| c.into()).collect::<Vec<Card>>();
+    let cards = hand.chars().map(parse_card2).collect::<Vec<Card>>();
     let mut cards_by_type: BTreeMap<Card, u8> = BTreeMap::new();
 
     for card in cards.clone() {
@@ -94,87 +104,87 @@ fn parse_card2(s: &str) -> Hand {
         *c += 1;
     }
 
-    let jokers = cards_by_type.get(&Card::JackJoker).unwrap_or(&0);
+    let jokers = cards_by_type.get(&Card::Joker).unwrap_or(&0);
     let cc = cards_by_type.values().cloned().collect::<Vec<_>>();
 
     // Convert cards and number of jokers to hand type
     // Upgrade to the strongest possible hand
-    let hand_type =
-        match (&cc[..], jokers) {
-            ([5], _) => HandType::FiveOfAKind,
+    let hand_type = match (&cc[..], jokers) {
+        ([5], _) => HandType::FiveOfAKind,
 
-            ([1, 1, 1, 1, 1], 0) => HandType::HighCard,
+        ([1, 1, 1, 1, 1], 0) => HandType::HighCard,
 
-            ([2, 1, 1, 1], 0) => HandType::OnePair,
-            ([1, 2, 1, 1], 0) => HandType::OnePair,
-            ([1, 1, 2, 1], 0) => HandType::OnePair,
-            ([1, 1, 1, 2], 0) => HandType::OnePair,
+        ([2, 1, 1, 1], 0) => HandType::OnePair,
+        ([1, 2, 1, 1], 0) => HandType::OnePair,
+        ([1, 1, 2, 1], 0) => HandType::OnePair,
+        ([1, 1, 1, 2], 0) => HandType::OnePair,
 
-            ([2, 1, 2], 0) => HandType::TwoPairs,
-            ([1, 2, 2], 0) => HandType::TwoPairs,
-            ([2, 2, 1], 0) => HandType::TwoPairs,
+        ([2, 1, 2], 0) => HandType::TwoPairs,
+        ([1, 2, 2], 0) => HandType::TwoPairs,
+        ([2, 2, 1], 0) => HandType::TwoPairs,
 
-            ([3, 1, 1], 0) => HandType::ThreeOfAKind,
-            ([1, 3, 1], 0) => HandType::ThreeOfAKind,
-            ([1, 1, 3], 0) => HandType::ThreeOfAKind,
+        ([3, 1, 1], 0) => HandType::ThreeOfAKind,
+        ([1, 3, 1], 0) => HandType::ThreeOfAKind,
+        ([1, 1, 3], 0) => HandType::ThreeOfAKind,
 
-            ([3, 2], 0) => HandType::FullHouse,
-            ([2, 3], 0) => HandType::FullHouse,
+        ([3, 2], 0) => HandType::FullHouse,
+        ([2, 3], 0) => HandType::FullHouse,
 
-            ([4, 1], 0) => HandType::FourOfAKind,
-            ([1, 4], 0) => HandType::FourOfAKind,
+        ([4, 1], 0) => HandType::FourOfAKind,
+        ([1, 4], 0) => HandType::FourOfAKind,
 
-            // 1234J -> 12344
-            ([1, 1, 1, 1, 1], 1) => HandType::OnePair,
+        // 1234J -> 12344
+        ([1, 1, 1, 1, 1], 1) => HandType::OnePair,
 
-            // KKJ12 -> KKK99
-            ([2, 1, 1, 1], 1) => HandType::ThreeOfAKind,
-            ([1, 2, 1, 1], 1) => HandType::ThreeOfAKind,
-            ([1, 1, 2, 1], 1) => HandType::ThreeOfAKind,
-            ([1, 1, 1, 2], 1) => HandType::ThreeOfAKind,
+        // KKJ12 -> KKK99
+        ([2, 1, 1, 1], 1) => HandType::ThreeOfAKind,
+        ([1, 2, 1, 1], 1) => HandType::ThreeOfAKind,
+        ([1, 1, 2, 1], 1) => HandType::ThreeOfAKind,
+        ([1, 1, 1, 2], 1) => HandType::ThreeOfAKind,
 
-            // KKJQQ -> KKKQQ
-            ([2, 1, 2], 1) => HandType::FullHouse,
-            ([1, 2, 2], 1) => HandType::FullHouse,
-            ([2, 2, 1], 1) => HandType::FullHouse,
+        // KKJQQ -> KKKQQ
+        ([2, 1, 2], 1) => HandType::FullHouse,
+        ([1, 2, 2], 1) => HandType::FullHouse,
+        ([2, 2, 1], 1) => HandType::FullHouse,
 
-            // 333J1 -> 33331
-            ([3, 1, 1], 1) => HandType::FourOfAKind,
-            ([1, 3, 1], 1) => HandType::FourOfAKind,
-            ([1, 1, 3], 1) => HandType::FourOfAKind,
+        // 333J1 -> 33331
+        ([3, 1, 1], 1) => HandType::FourOfAKind,
+        ([1, 3, 1], 1) => HandType::FourOfAKind,
+        ([1, 1, 3], 1) => HandType::FourOfAKind,
 
-            // 3333J -> 33333
-            ([4, 1], 1) => HandType::FiveOfAKind,
-            ([1, 4], 1) => HandType::FiveOfAKind,
+        // 3333J -> 33333
+        ([4, 1], 1) => HandType::FiveOfAKind,
+        ([1, 4], 1) => HandType::FiveOfAKind,
 
-            // 123JJ -> 12333
-            ([2, 1, 1, 1], 2) => HandType::ThreeOfAKind,
-            ([1, 2, 1, 1], 2) => HandType::ThreeOfAKind,
-            ([1, 1, 2, 1], 2) => HandType::ThreeOfAKind,
-            ([1, 1, 1, 2], 2) => HandType::ThreeOfAKind,
+        // 123JJ -> 12333
+        ([2, 1, 1, 1], 2) => HandType::ThreeOfAKind,
+        ([1, 2, 1, 1], 2) => HandType::ThreeOfAKind,
+        ([1, 1, 2, 1], 2) => HandType::ThreeOfAKind,
+        ([1, 1, 1, 2], 2) => HandType::ThreeOfAKind,
 
-            // KKJJQ -> KKKKQ
-            ([2, 1, 2], 2) => HandType::FourOfAKind,
-            ([1, 2, 2], 2) => HandType::FourOfAKind,
-            ([2, 2, 1], 2) => HandType::FourOfAKind,
+        // KKJJQ -> KKKKQ
+        ([2, 1, 2], 2) => HandType::FourOfAKind,
+        ([1, 2, 2], 2) => HandType::FourOfAKind,
+        ([2, 2, 1], 2) => HandType::FourOfAKind,
 
-            // 333JJ -> 33333
-            ([3, 1, 1], 2) => HandType::FiveOfAKind,
-            ([1, 3, 1], 2) => HandType::FiveOfAKind,
-            ([1, 1, 3], 2) => HandType::FiveOfAKind,
+        // 333JJ -> 33333
+        ([3, 1, 1], 2) => HandType::FiveOfAKind,
+        ([1, 3, 1], 2) => HandType::FiveOfAKind,
+        ([1, 1, 3], 2) => HandType::FiveOfAKind,
 
-            // Misc found while running real data
-            ([3, 1, 1], 3) => HandType::FourOfAKind,
-            ([1, 3, 1], 3) => HandType::FourOfAKind,
-            ([2, 3], 2) => HandType::FiveOfAKind,
-            ([3, 2], 2) => HandType::FiveOfAKind,
-            ([3, 2], 3) => HandType::FiveOfAKind,
-            ([2, 3], 3) => HandType::FiveOfAKind,
-            ([4, 1], 4) => HandType::FiveOfAKind,
-            ([1, 4], 4) => HandType::FiveOfAKind,
+        // Misc found while running real data
+        ([3, 1, 1], 3) => HandType::FourOfAKind,
+        ([1, 3, 1], 3) => HandType::FourOfAKind,
+        ([1, 1, 3], 3) => HandType::FourOfAKind,
+        ([2, 3], 2) => HandType::FiveOfAKind,
+        ([3, 2], 2) => HandType::FiveOfAKind,
+        ([3, 2], 3) => HandType::FiveOfAKind,
+        ([2, 3], 3) => HandType::FiveOfAKind,
+        ([4, 1], 4) => HandType::FiveOfAKind,
+        ([1, 4], 4) => HandType::FiveOfAKind,
 
-            h => todo!("{:?}", h),
-        };
+        h => todo!("{:?}", h),
+    };
 
     Hand {
         cards,
@@ -185,14 +195,14 @@ fn parse_card2(s: &str) -> Hand {
 
 #[aoc_generator(day7, part1)]
 pub fn input_generator(input: &str) -> Input {
-    let hands = input.lines().map(parse_card).collect();
+    let hands = input.lines().map(parse_hand).collect();
 
     Input { hands }
 }
 
 #[aoc_generator(day7, part2)]
 pub fn input_generator_part2(input: &str) -> Input {
-    let hands = input.lines().map(parse_card2).collect();
+    let hands = input.lines().map(parse_hand2).collect();
 
     Input { hands }
 }
@@ -252,7 +262,7 @@ pub fn solve_part_01(input: &Input) -> u64 {
 /// ```
 /// use advent_of_code_2023::day_07::*;
 /// let data = include_str!("../input/2023/day7.txt");
-/// assert_eq!(solve_part_02(&input_generator(data)), 28101347);
+/// assert_eq!(solve_part_02(&input_generator_part2(data)), 28101347);
 /// ```
 */
 #[aoc(day7, part2)]
