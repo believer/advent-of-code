8eb9443847e7483dccabd6eb0af715c502705db6
believer <rickard@nattochdag.dev>
2023-12-08 06:20:11 +0100

    feat(2023): add day 8 part 1

--- rust/2023/src/day_08.rs
+++ rust/2023/src/day_08.rs
@@ -1,14 +1,40 @@
+use std::collections::HashMap;
+
 // Day 8:
 
 #[derive(Debug)]
 pub struct Input {
-    data: u64,
+    right_left: String,
+    instructions: HashMap<String, (String, String)>,
 }
 
 #[aoc_generator(day8)]
 pub fn input_generator(input: &str) -> Input {
-    println!("{}", input);
-    Input { data: 0 }
+    let mut input = input.split("\n\n");
+
+    let right_left = input.next().unwrap();
+    let instructions = input
+        .next()
+        .unwrap()
+        .lines()
+        .map(|line| {
+            let mut line = line.split(" = ");
+            let start = line.next().unwrap().to_string();
+            let map = line
+                .next()
+                .unwrap()
+                .split_once(", ")
+                .map(|(x, y)| (x.replace(['(', ')'], ""), y.replace([')', ')'], "")))
+                .unwrap();
+
+            (start, map)
+        })
+        .collect::<HashMap<String, (String, String)>>();
+
+    Input {
+        right_left: right_left.to_string(),
+        instructions,
+    }
 }
 
 /* Part One
@@ -25,7 +51,27 @@ pub fn input_generator(input: &str) -> Input {
 */
 #[aoc(day8, part1)]
 pub fn solve_part_01(input: &Input) -> u64 {
-    input.data
+    let mut steps = 0;
+    let mut location = "AAA".to_string();
+
+    while location != "ZZZ" {
+        for char in input.right_left.chars() {
+            match char {
+                'R' => {
+                    let (_, right) = input.instructions.get(&location).unwrap();
+                    location = right.to_string();
+                }
+                'L' => {
+                    let (left, _) = input.instructions.get(&location).unwrap();
+                    location = left.to_string();
+                }
+                _ => unreachable!(),
+            }
+            steps += 1;
+        }
+    }
+
+    steps
 }
 
 /* Part Two
@@ -48,10 +94,28 @@ pub fn solve_part_02(_input: &Input) -> u64 {
 mod tests {
     use super::*;
 
-    const DATA: &str = "";
+    const DATA: &str = "RL
+
+AAA = (BBB, CCC)
+BBB = (DDD, EEE)
+CCC = (ZZZ, GGG)
+DDD = (DDD, DDD)
+EEE = (EEE, EEE)
+GGG = (GGG, GGG)
+ZZZ = (ZZZ, ZZZ)";
 
     #[test]
     fn sample_01() {
-        assert_eq!(solve_part_01(&input_generator(DATA)), 0);
+        assert_eq!(solve_part_01(&input_generator(DATA)), 2);
+    }
+
+    fn sample_02() {
+        let data = "LLR
+
+AAA = (BBB, BBB)
+BBB = (AAA, ZZZ)
+ZZZ = (ZZZ, ZZZ)";
+
+        assert_eq!(solve_part_02(&input_generator(data)), 6);
     }
 }
