26923669519fd0e6ef190ac460a6daf98913579c
Rickard Natt och Dag <rickard@nattochdag.dev>
2023-12-18 15:16:00 +0100

    chore(2023): disable day 19 doc tests

--- rust/2023/src/day_19.rs
+++ rust/2023/src/day_19.rs
@@ -15,12 +15,14 @@ pub fn input_generator(input: &str) -> Input {
 /* Part One
 *
 */
+/*
 // Your puzzle answer was
 #[doc = r#"```
 use advent_of_code_2023::day_19::*;
 let data = include_str!("../input/2023/day19.txt");
 assert_eq!(solve_part_01(&input_generator(data)), 0);
 ```"#]
+*/
 #[aoc(day19, part1)]
 pub fn solve_part_01(input: &Input) -> u32 {
     input.data
@@ -30,11 +32,13 @@ pub fn solve_part_01(input: &Input) -> u32 {
 *
 *
 */
+/*
 #[doc = r#"```
 use advent_of_code_2023::day_19::*;
 let data = include_str!("../input/2023/day19.txt");
 assert_eq!(solve_part_02(&input_generator(data)), 0);
 ```"#]
+*/
 #[aoc(day19, part2)]
 pub fn solve_part_02(_input: &Input) -> u32 {
     todo!()
