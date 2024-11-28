37126abad0a6f5959ad5737c5b50a340f6a1d7c0
Rickard Natt och Dag <rickard@nattochdag.dev>
2024-11-28 19:07:09 +0100

    chore(2024): add some utils that might be needed

---
+++ go/2024/utils/utils.go
@@ -0,0 +1,53 @@
+package utils
+
+import "strconv"
+
+type Point struct {
+	x int
+	y int
+}
+
+// Ints
+// -----------------------------------------------------------
+
+func Sum(nums []int) int {
+	total := 0
+
+	for _, n := range nums {
+		total += n
+	}
+
+	return total
+}
+
+func FromString(s string) int {
+	v, err := strconv.Atoi(s)
+
+	if err != nil {
+		panic(err)
+	}
+
+	return v
+}
+
+// Math
+// -----------------------------------------------------------
+
+// For float64, use math.Abs
+func Abs(n int) int {
+	if n < 0 {
+		return -n
+	}
+
+	return n
+}
+
+// Manhattan distance
+// The sum of the absolute values of two points
+// This function can be used when the starting point can be considered
+// the origin (0,0). The original formula is
+//
+// |x1 - x2| + \y1 - y2|
+func Manhattan(x, y int) int {
+	return Abs(x) + Abs(y)
+}
