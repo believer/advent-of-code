25105f8f409b53dec797a3212174d097c0d64c51
Rickard Natt och Dag <rickard@nattochdag.dev>
2024-12-01 06:16:26 +0100

    feat(2024): day 1

---
+++ go/2024/puzzles/day01/main.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"fmt"
+	"slices"
+	"strconv"
+	"strings"
+
+	"github.com/believer/aoc-2024/utils"
+	"github.com/believer/aoc-2024/utils/files"
+)
+
+func main() {
+	fmt.Println("Part 1: ", part1("input.txt"))
+	fmt.Println("Part 2: ", part2("input.txt"))
+}
+
+func part1(name string) int {
+	lines := files.ReadLines(name)
+	left := []int{}
+	right := []int{}
+	total := 0
+
+	for _, line := range lines {
+		row := strings.Split(line, "   ")
+
+		l, err := strconv.Atoi(row[0])
+
+		if err != nil {
+			panic(err)
+		}
+
+		r, err := strconv.Atoi(row[1])
+
+		if err != nil {
+			panic(err)
+		}
+
+		left = append(left, l)
+		right = append(right, r)
+	}
+
+	slices.Sort(left)
+	slices.Sort(right)
+
+	for i, l := range left {
+		total += utils.Abs(l - right[i])
+	}
+
+	return total
+}
+
+func part2(name string) int {
+	lines := files.ReadLines(name)
+	left := []int{}
+	right := []int{}
+	total := 0
+
+	for _, line := range lines {
+		row := strings.Split(line, "   ")
+
+		l, err := strconv.Atoi(row[0])
+
+		if err != nil {
+			panic(err)
+		}
+
+		r, err := strconv.Atoi(row[1])
+
+		if err != nil {
+			panic(err)
+		}
+
+		left = append(left, l)
+		right = append(right, r)
+	}
+
+	for _, l := range left {
+		appears := 0
+		for _, r := range right {
+			if l == r {
+				appears += 1
+			}
+		}
+
+		total += l * appears
+	}
+
+	return total
+}
