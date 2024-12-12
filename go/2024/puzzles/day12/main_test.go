e82067e08ab6c87e695dcbf21e7f588a2b420014
Rickard Natt och Dag <rickard@nattochdag.dev>
2024-12-12 17:42:44 +0100

    test: add all test cases for day 12

--- go/2024/puzzles/day12/main_test.go
+++ go/2024/puzzles/day12/main_test.go
@@ -38,6 +38,38 @@ func TestPart2(t *testing.T) {
 	})
 }
 
+func TestPart2MediumFromPart1(t *testing.T) {
+	t.Run("Part 1", func(t *testing.T) {
+		expected := 436
+		actual := part2("test-input-medium.txt")
+		assert.Equal(t, expected, actual)
+	})
+}
+
+func TestPart2EShape(t *testing.T) {
+	t.Run("Part 2", func(t *testing.T) {
+		expected := 236
+		actual := part2("test-input-e.txt")
+		assert.Equal(t, expected, actual)
+	})
+}
+
+func TestPart2Medium(t *testing.T) {
+	t.Run("Part 2", func(t *testing.T) {
+		expected := 368
+		actual := part2("test-input-medium-part2.txt")
+		assert.Equal(t, expected, actual)
+	})
+}
+
+func TestPart2Large(t *testing.T) {
+	t.Run("Part 2", func(t *testing.T) {
+		expected := 1206
+		actual := part2("test-input-large.txt")
+		assert.Equal(t, expected, actual)
+	})
+}
+
 func BenchmarkPart1(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		part1("input.txt")
