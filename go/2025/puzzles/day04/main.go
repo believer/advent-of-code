313738b4d38cd82c3d4b7f9615b8d76ade148a7d
Rickard Natt och Dag <rickard@nattochdag.dev>
2025-12-04 18:04:41 +0100

    refactor(2025): clean up day 4

--- go/2025/puzzles/day04/main.go
+++ go/2025/puzzles/day04/main.go
@@ -15,7 +15,36 @@ func main() {
 func part1(name string) int {
 	lines := files.ReadLines(name)
 	department := grid.New(lines)
-	accessible := 0
+
+	return len(findAccessible(department))
+}
+
+func part2(name string) int {
+	lines := files.ReadLines(name)
+	department := grid.New(lines)
+	removable := 0
+
+	for {
+		accessible := findAccessible(department)
+
+		// No more paper to remove
+		if len(accessible) == 0 {
+			break
+		}
+
+		// Remove the paper
+		for _, p := range accessible {
+			department.Update(p, '.')
+		}
+
+		removable += len(accessible)
+	}
+
+	return removable
+}
+
+func findAccessible(department grid.Grid) []grid.Point {
+	accessible := []grid.Point{}
 
 	for y := range department.Height {
 		for x := range department.Width {
@@ -40,62 +69,10 @@ func part1(name string) int {
 			}
 
 			if rolls < 4 {
-				accessible += 1
+				accessible = append(accessible, p)
 			}
 		}
 	}
 
 	return accessible
-}
-
-func part2(name string) int {
-	lines := files.ReadLines(name)
-	department := grid.New(lines)
-	removable := 0
-
-	for {
-		accessible := []grid.Point{}
-
-		for y := range department.Height {
-			for x := range department.Width {
-				rolls := 0
-				p := grid.Point{X: x, Y: y}
-
-				// No paper here
-				if department.Get(p) != '@' {
-					continue
-				}
-
-				for _, dir := range grid.ALL_DIRECTIONS {
-					check := grid.Point{X: p.X + dir.X, Y: p.Y + dir.Y}
-
-					if _, ok := department.Contains(check); !ok {
-						continue
-					}
-
-					if department.Get(check) == '@' {
-						rolls += 1
-					}
-				}
-
-				if rolls < 4 {
-					accessible = append(accessible, p)
-				}
-			}
-		}
-
-		// No more paper to remove
-		if len(accessible) == 0 {
-			break
-		}
-
-		// Remove the paper
-		for _, p := range accessible {
-			department.Update(p, '.')
-		}
-
-		removable += len(accessible)
-	}
-
-	return removable
 }
