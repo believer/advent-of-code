3dd94ddd7fe608c4062cee007631c997246eec36
Rickard Natt och Dag <rickard@nattochdag.dev>
2025-12-04 06:40:14 +0100

    feat(2025): day 4 part 2

--- go/2025/puzzles/day04/main.go
+++ go/2025/puzzles/day04/main.go
@@ -49,5 +49,53 @@ func part1(name string) int {
 }
 
 func part2(name string) int {
-	return 0
+	lines := files.ReadLines(name)
+	department := grid.New(lines)
+	removable := 0
+
+	for {
+		accessible := []grid.Point{}
+
+		for y := range department.Height {
+			for x := range department.Width {
+				rolls := 0
+				p := grid.Point{X: x, Y: y}
+
+				// No paper here
+				if department.Get(p) != '@' {
+					continue
+				}
+
+				for _, dir := range grid.ALL_DIRECTIONS {
+					check := grid.Point{X: p.X + dir.X, Y: p.Y + dir.Y}
+
+					if _, ok := department.Contains(check); !ok {
+						continue
+					}
+
+					if department.Get(check) == '@' {
+						rolls += 1
+					}
+				}
+
+				if rolls < 4 {
+					accessible = append(accessible, p)
+				}
+			}
+		}
+
+		// No more paper to remove
+		if len(accessible) == 0 {
+			break
+		}
+
+		// Remove the paper
+		for _, p := range accessible {
+			department.Update(p, '.')
+		}
+
+		removable += len(accessible)
+	}
+
+	return removable
 }
